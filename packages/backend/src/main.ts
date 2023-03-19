ea56ab4e49691ce9e74aa057835e67c9f4da5d87
Loïc Vanden Bossche <loloic066@gmail.com>
2023-03-19 04:20:49 +0100

    Added global prefix for prod

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+ // Only in production
+  app.setGlobalPrefix('aennesia/api');
+
   await app.listen(3001);
 }
 bootstrap();
